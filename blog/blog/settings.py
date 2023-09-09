fbb159784f8a85cb1607dd8cb03d42f4e642f227
unknown <2046823043@qq.com>
2023-09-09 22:00:19 +0800

    'createproject'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "PORT":3306,
         "USER":'bk',
         "PASSWORD":'123456',
-        'NAME': 'bk_blog'
+        'NAME': 'bk_blog',
     }
 }
 
@@ -122,3 +122,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#redis配置
+CACHES = {
+    "default":{
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":"redis://127.0.0.1:6379/0",
+        "OPTIONS":{
+            "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    },
+
+    "session":{
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":"redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+#session由数据库存储改为redis
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
+
